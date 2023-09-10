e15ba4640e2f052df3db39795792cabe2070723a
UVQoZKHshXS <dumx@eastcom-sw.com>
2023-09-10 18:50:17 +0800

    day1

--- xuecheng-plus-generator/src/main/java/com/xuecheng/generator/ContentCodeGenerator.java
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/generator/ContentCodeGenerator.java
@@ -21,7 +21,7 @@ public class ContentCodeGenerator {
 	//数据库账号
 	private static final String DATA_SOURCE_USER_NAME  = "root";
 	//数据库密码
-	private static final String DATA_SOURCE_PASSWORD  = "mysql";
+	private static final String DATA_SOURCE_PASSWORD  = "123456";
 	//生成的表
 	private static final String[] TABLE_NAMES = new String[]{
 //			"mq_message",
@@ -67,7 +67,7 @@ public class ContentCodeGenerator {
 		// 数据库配置
 		DataSourceConfig dsc = new DataSourceConfig();
 		dsc.setDbType(DbType.MYSQL);
-		dsc.setUrl("jdbc:mysql://192.168.101.65:3306/xc402_" + SERVICE_NAME
+		dsc.setUrl("jdbc:mysql://120.55.166.219:3311/xc402_" + SERVICE_NAME
 				+ "?serverTimezone=UTC&useUnicode=true&useSSL=false&characterEncoding=utf8");
 //		dsc.setDriverName("com.mysql.jdbc.Driver");
 		dsc.setDriverName("com.mysql.cj.jdbc.Driver");
